949601797e2d380ce070c6358579d97a1ef374cd
gostaar <bauwens.delphine@gmail.com>
2024-12-08 21:20:53 +0100

    mise en page

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,40 @@ window.addEventListener('DOMContentLoaded', () => {
     const status = document.getElementById('session.status');
     const isAuthenticated = (status.textContent.trim() === 'connecté');
     if (!isAuthenticated) {clearData() };
+
+    const proButton = document.getElementById('btnPro');
+    const partButton = document.getElementById('btnPart');
+
+    if (proButton && partButton) {
+        proButton.addEventListener('click', () => toggleSection('pro'));
+        partButton.addEventListener('click', () => toggleSection('part'));
+    } else {
+        console.warn('Les boutons "btnPro" ou "btnPart" sont introuvables.');
+    }
 });
+
+window.addEventListener('hashchange', function () {
+    const hash = location.hash.replace('#', '');
+    showSection(hash);
+});
+
+function updateURLWithoutHash() {
+    history.replaceState(null, '', window.location.pathname);
+}
+
+function showSection(targetId) {
+    
+    const allSections = document.querySelectorAll('[id^="link-"]');
+    
+    allSections.forEach(section => {
+        if (section.id === targetId) {
+            section.classList.remove('d-none');
+        } else {
+            section.classList.add('d-none');
+        }
+    });
+    history.replaceState(null, '', window.location.pathname);
+}
 
 function clearData(){
     sessionStorage.clear();
@@ -12,4 +45,25 @@ function clearData(){
         const cookieName = cookie.split('=')[0].trim();
         document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/, secure, sameSite=None`;
     });
+}
+
+function toggleSection(targetId) {
+    const sections = ['pro', 'part']; 
+    const btnPartPro = document.getElementById('btnPartPro');
+
+    sections.forEach(sectionId => {
+        const section = document.getElementById(sectionId);
+        if (sectionId === targetId) {
+            section.classList.remove('d-none'); 
+            section.classList.add('fade-in'); 
+            location.replace(`/#link-start-${targetId}`);
+        } else {
+            section.classList.add('d-none'); 
+            section.classList.remove('fade-in');
+        }
+    });
+
+    if (btnPartPro) {
+        btnPartPro.classList.add('d-none');
+    }
 }
